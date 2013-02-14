e67b26e6c75007d1c701293f1a8cd4e5f5eed528
steve ulrich <sulrich@botwerks.org>
2013-02-14 14:03:52 -0600

    output generators now work

--- filter-parse.pl
+++ filter-parse.pl
@@ -1,48 +1,74 @@
 #!/usr/bin/env perl
 
+
+# steve ulrich <sulrich@cisco.com>
+#
+# script to conver a junos filter to an ios-xr object-group notation
+# style ACL.  i'm sure stuff's broke in here.
+
 use strict;
 use warnings;
 use Data::Dumper;
 use Text::Balanced qw(extract_multiple extract_delimited extract_bracketed);
 
 
-my %term_fields = (
+my %term_fields = (             # bracketed term fields
                    "destination-address"  => "",
                    "source-address"       => "",
-                   # "from"                 => "",
+                   # "from"               => "",
                    "then"                 => "",
                   );
 
-my %term_atoms = (
-                   "protocol"             => "",
-                   "destination-port"     => "",
-                   "source-port"          => "",
-                   "forwarding-class"     => "",
-                   "count"                => "",
-                   "accept"               => "",
-                   "tcp-established"      => "",
+my %term_atoms = (              # these are field terms we attempt to do
+                                # something reasonable with
+                   "protocol"         => "",
+                   "destination-port" => "",
+                   "source-port"      => "",
+                   "forwarding-class" => "",
+                   "count"            => "",
+                   "accept"           => "",
+                   "tcp-established"  => "",
+                   "packet-length"    => "",
                   );
 
 
-my @terms = (); # stick all of the fw filter terms we run into in here.
+my @terms       = (); # all of the fw filter terms go in here.
+my $term        = {}; # anonymous hash for pushing into @terms
+my $term_cap    = 0;  # flag for capturing terms
+my $term_name   = ""; # key for the term
+my $filter_name = "";
+my $c_net_obj   = "";
+my $c_port_obj  = "";
+my $c_acl_obj   = "";
+my $acl_incr    = 10; # amount to increment ACL line #s by
 
-my $term_cap  = 0;  # flag for capturing terms
-my $term_name = ""; # key for the term
-my $term      = {}; # anonymous hash for pushing into @terms
+
+# standard format for the translated objects is:
+#
+# -  object-group network ipv4 TERM_NAME-SRC/DST
+# -  object-group port TERM_NAME-SRC/DST
+#
+# junos filter name will result in the same acl name on the IOS XR side
 
 open(ACL, $ARGV[0]) || die "error opening: $ARGV[0]";
 while (<ACL>) {
+  if (/filter (\w\S+)\s+\{/i) {
+    $filter_name = $1;               # we happen to have this in the file,
+                                  # use it to name the acl
+  }
+
+
   if (( /term (\w\S+)\s+\{/i ) && ($term_cap == 0)) {
     # this is the first term we're running into
-    $term_cap          = 1; # start capturing
-    $term_name         = $1;
+    $term_cap  = 1;                          # start capturing
+    $term_name = $1;
 
   } elsif (( /term (\w\S+)\s+\{/ ) && ($term_cap == 1)) {
-    # we've bumped up against this is the next term for this filter the
-    # order of operations is important here
+    # we've bumped up against this is the next term for this filter
+    #
+    # the order of operations is important here!
     push @terms, $term;
-    $term = {};                            # reset this value
-
+    $term      = {};                  # reset this value
     $term_name = $1;
   }
 
@@ -50,58 +76,191 @@ while (<ACL>) {
     $term->{$term_name} .= $_;
   }
 }
-push @terms, $term;              # do cleanup prior to closing the file.
+push @terms, $term;              # cleanup prior to closing the file.
 close(ACL);
 
 
 
-foreach my $i (0 .. $#terms) {  # handle in order
+foreach my $i (0 .. $#terms) {       # handle terms in order
   foreach my $j (keys %{ $terms[$i] } ) {
-    print "term name: $j\n";
-    my $acl = &parseAclTerm($j, $terms[$i]{$j} );
-    print Dumper $acl;
-  }
+    my $acl = &parseAclTerm( $j, $terms[$i]{$j} );
 
+    my ($netobj, $portobj, $acl) = &processTerm($j, $acl);
+
+
+
+  }
 }
 
-sub parseAclTerm () {
+
+sub processTerm {
+  my ($aclname, $aclref) = @_;
+
+  my ($netobj, $portobj, $acl,
+      $src_addr, $src_ports,
+      $dst_addr, $dst_ports,
+      $action, $counter) = "";
+
+  my @protocols      = ('ip');
+  my $netobj_prefix  = "object-group network ipv4";
+  my $portobj_prefix = "object-group port ";
+  my $src_net        = "$aclname-SRC";
+  my $dst_net        = "$aclname-DST";
+  my $s_port_name    = "$aclname-SRC_PORTS";
+  my $d_port_name    = "$aclname-DST_PORTS";
+
+  foreach my $field (keys %{ $aclref->{$aclname} }  ) {
+
+    if ($field  =~ /destination-address/i ) {
+      my $dst_addr_block = &parseAddrBlock( $aclref->{$aclname}->{$field} );
+      $netobj .= $netobj_prefix . "$dst_net\n" . $dst_addr_block . "\n";
+    }
+
+    elsif ($field  =~ /source-address/i ) {
+      my $src_addr_block = &parseAddrBlock( $aclref->{$aclname}->{$field} );
+      $netobj .= $netobj_prefix . "$src_net\n" . $src_addr_block . "\n";
+    }
+
+    elsif ($field  =~ /protocol/i ) {
+     @protocols = &parseProtocol( $aclref->{$aclname}->{$field} );
+    }
+
+    elsif ($field  =~ /source-port/i )         {
+      $src_ports = &parsePortBlock( $aclref->{$aclname}->{$field} );
+      $portobj .= $portobj_prefix . $aclname . "-SRC\n" . $src_ports . "\n";
+    }
+
+    elsif ($field  =~ /destination-port/i )    {
+      $dst_ports = &parsePortBlock( $aclref->{$aclname}->{$field} );
+      $portobj .= $portobj_prefix . $aclname . "-DST\n" . $dst_ports . "\n";
+    }
+    elsif ($field  =~ /then/i ) {
+      $action = &parseAction( $aclref->{$aclname}->{$field} );
+    }
+  }
+
+  my ($s_p, $d_p) = "";
+
+  foreach my $prot (@protocols) {
+    $s_p = "port-group $s_port_name" if ($src_ports ne "");
+    $d_p = "port-group $d_port_name" if ($dst_ports ne "");
+
+    $acl .= "$action $prot net-group $src_net $s_p net-group $dst_net $d_p" . "\n";
+  }
+
+  return ($netobj, $portobj, $acl);
+}
+
+
+sub parseProtocol {
+  my ($str) = @_;
+  $str =~ s/\[|\]|\;//g; # rip off the brackets
+  $str =~ s/^\s+//g;
+  $str =~ s/\s+$//g;     # cleanup whitespace
+
+  my @protocols = split(/\s+/, $str);
+  return @protocols;
+}
+
+sub parsePortBlock {
+  my ($str) = @_;
+  $str =~ s/\[|\]|\;//g; # rip off the brackets
+  $str =~ s/^\s+//g;
+  $str =~ s/\s+$//g;     # cleanup whitespace
+
+  my @ports = split(/\s+/, $str);
+
+  my $portblock = "";
+  foreach my $p (@ports) {
+    #$p =~ s/\s//g;      # remove any white space
+
+    if ($p =~ /(\d+)\-(\d+)/) {
+      $portblock .= "  range $1 $2\n"
+    } else {
+      $portblock .= "  eq $p\n"
+    }
+  }
+  return $portblock;
+}
+
+
+# generically manipulate junos prefix lists
+sub parseAddrBlock {
+  my ($addrs) = @_;
+  my $block = "";
+
+  my @addrblock = split(/\n/, $addrs);
+
+  foreach my $pref (@addrblock) {
+    $pref =~ s/^\s+//g;
+    $pref =~ s/\s+$//g;     # cleanup exteraneous whitespace
+    $pref =~ s/\;//g;
+    next if ($pref eq "");
+
+    $pref   = "  !! $pref" if ( /except/i); # oh what to do about exceptions...?
+    $block .= "  $pref\n";
+  }
+  return $block;
+}
+
+# parse the 'then' component of the junos term - there's room for a lot more thought here
+sub parseAction {
+  my ($actions) = @_;
+  my $act = "permit";
+
+  foreach my $opt ($actions) {
+    $opt =~ s/^\s+//g;
+    $opt =~ s/\s+$//g;     # cleanup exteraneous whitespace
+    $opt =~ s/\;//g;
+
+    $act = "deny" if $opt =~ /discard/i;
+  }
+
+  return $act;
+}
+
+
+
+
+# ---------------------------------------------------------------------
+# parseJunosAclTerm - returns a HoH which contain all of the elements
+# associated with the junos filter term passed to this.
+#
+sub parseAclTerm {
   my ($name, $content) = @_;
   my $parse_flag = 0;
-
-  my $acl = {};    # initialize anonymous for return value
-
+  my $acl        = {};                    # initialize anonymous for return value
 
   while ($parse_flag == 0) {
     my ($pref, $val, $rem) = &get_bracketed($content);
     $parse_flag = 1 if ($val eq "" && $rem eq "");
-    $content = $val . $rem; # does this make sense?
+    $content = $val . $rem;                # does this make sense?
 
-    next if $pref =~ /$name/i; # skip the filter term name
-    # next if $pref =~ /from/i;  #
+    next if $pref =~ /$name/i;             # skip the filter term name
 
-    # some fields get stranded in a strictly nested parsing model
-    # break these out separately - specifically in the from{} portion of the config
+    # some fields get stranded in a strictly nested parsing model these
+    # need to be addressed separately - specifically in the from{}
+    # portion of the config
     if ($pref =~ /from/i) {
       # iterate through $val for matches
       # c - continue
       # i - case insensitive
       # g - global
-      while ( $val =~ /\b([\-\w]+)\s+(.+)\;/gci ) {
+      while ( $val =~ /\b([\-\w]+)\s+(.+)\;/gci ) { # we only want "words" here
         if (exists $term_atoms{$1} ) {
           $acl->{$name}{$1} = $2;
         } else {
           next if $2 =~ /except/;  # junos' "except" is a slick, except for here
-          print "error: new atom ($1 - $2)\n";
+          print "!! error: new atom ($1 - $2)\n";
         }
       }
     }
 
     if ( exists $term_fields{$pref} ) {
-      $val =~ s/\{|\}//g;
+      $val =~ s/\{|\}//g;           # strip brackets
       $acl->{$name}{$pref} = $val;
     }
   }
-
   return $acl;
 }
 
@@ -112,19 +271,14 @@ sub get_bracketed {
   # seek to beginning of bracket
   return undef unless $str =~ /(\S+)\s+(?={)/gc;
 
-  # store the prefix
   my $prefix = $1;
-  #print "prefix: $prefix\n";
 
   # get everything from the start brace to the matching end brace
   my ($content, $remainder) = extract_bracketed( $str, '{}');
-  #print "  content: $content\n";
-  #print "remainder: $remainder\n";
 
   # no closing brace found
   return undef unless $content;
 
-  # return the whole match
   return($prefix, $content, $remainder);
 
 }
